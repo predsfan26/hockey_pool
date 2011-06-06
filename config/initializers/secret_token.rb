f81637f49105a4c0766c3808f796a7c2079bb995
David Matthews <predsfan26@gmail.com>
2011-06-05 19:59:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HockeyPool::Application.config.secret_token = '349262e6f3a6aefb95b9c5534957f117a43a545ff2e065eca1c02549aae190da02948da72890527a4358fde3f3716f9e2226f2dfa76b72890ab757ea42c8a3a5'
